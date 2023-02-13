9c6b4b1007b364c8eec22e7e063e6c753e78ace1
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2023-02-13 10:53:09 +0100

    feat: add ability to set multiple resources for a single backup plan

--- alabcdk/backup.py
+++ alabcdk/backup.py
@@ -1,7 +1,12 @@
 from constructs import Construct
-from aws_cdk import (aws_backup, aws_events, aws_iam)
-import aws_cdk as cdk
+from aws_cdk import (
+    aws_backup as backup,
+    aws_events as events,
+    aws_iam as iam,
+    Duration
+)
 from .utils import gen_name
+from __future__ import annotations
 
 '''
 Example how to use this module
@@ -19,7 +24,7 @@ backup.add_backup_rule(cron_expression="15 14 * * ? *", retentation_period_days=
 '''
 
 
-class BackupPlan(aws_backup.BackupPlan):
+class BackupPlan(backup.BackupPlan):
 
     def __init__(
             self,
@@ -27,11 +32,12 @@ class BackupPlan(aws_backup.BackupPlan):
             id: str,
             *,
             backup_resource_arn: str = None,
-            vault: aws_backup.BackupVault = None,
+            backup_resource_arns: list[str] = None,
+            vault: backup.BackupVault = None,
             PITR_retention_period_days: int = None,
             **kwargs) -> None:
         if vault is None:
-            vault = aws_backup.BackupVault(
+            vault = backup.BackupVault(
                 scope,
                 gen_name(scope, f"{id}-backup-vault"),
                 backup_vault_name=f"{id}-backup-vault")
@@ -41,6 +47,7 @@ class BackupPlan(aws_backup.BackupPlan):
 
         params
         backup_resource_arn: the arn of the resource to be backup
+        backup_resource_arns: a list of arns of the resource to be backup
         vault: The backup vault to be used to store the backup. If None then a vault is created
         PITR_retention_period_days: If set, then point-in-time recovery (PITR) is enabled. Set the
         number of days to keep data.
@@ -52,26 +59,32 @@ class BackupPlan(aws_backup.BackupPlan):
 
         """
         if PITR_retention_period_days is not None:
-            self.add_rule(aws_backup.BackupPlanRule(
+            self.add_rule(backup.BackupPlanRule(
                 rule_name=f"{id}-PITR-rule",
                 enable_continuous_backup=True,
-                delete_after=cdk.Duration.days(PITR_retention_period_days)))
+                delete_after=Duration.days(PITR_retention_period_days)))
 
-        if backup_resource_arn is None:
+        if backup_resource_arn is None and backup_resource_arns is None:
             raise ValueError("One need to specify a backup resource!")
+        if backup_resource_arns is not None:
+            resource_list = [backup.BackupResource.from_arn(arn) for arn in backup_resource_arns]
+        else:
+            resource_list = []
+        if backup_resource_arn is not None:
+            resource_list.append(backup.BackupResource.from_arn(backup_resource_arn))
 
         # Create a role to attach to the selections resources
-        self.role = aws_iam.Role(
+        self.role = iam.Role(
             self,
             f"{id}-aws-backup-role",
-            assumed_by=aws_iam.ServicePrincipal("backup.amazonaws.com"),
+            assumed_by=iam.ServicePrincipal("backup.amazonaws.com"),
             managed_policies=[
-                aws_iam.ManagedPolicy.from_managed_policy_arn(
+                iam.ManagedPolicy.from_managed_policy_arn(
                     self,
                     f"{id}-mp-s3-backup",
                     managed_policy_arn="arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Backup",
                 ),
-                aws_iam.ManagedPolicy.from_managed_policy_arn(
+                iam.ManagedPolicy.from_managed_policy_arn(
                     self,
                     f"{id}-mp-s3-restore",
                     managed_policy_arn="arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Restore",
@@ -80,8 +93,8 @@ class BackupPlan(aws_backup.BackupPlan):
         )
 
         self.add_selection(
-            f"{id}-backup-resource",
-            resources=[aws_backup.BackupResource.from_arn(backup_resource_arn)],
+            f"{id}-backup-resources",
+            resources=resource_list,
             role=self.role,
             allow_restores=True)
 
@@ -91,7 +104,7 @@ class BackupPlan(aws_backup.BackupPlan):
         https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents-expressions.html
         e.g. "37 13 * * ? *"
         '''
-        schedule = aws_events.Schedule.expression(f"cron({cron_expression})")
-        self.add_rule(aws_backup.BackupPlanRule(
+        schedule = events.Schedule.expression(f"cron({cron_expression})")
+        self.add_rule(backup.BackupPlanRule(
             schedule_expression=schedule,
-            delete_after=cdk.Duration.days(retentation_period_days)))
+            delete_after=Duration.days(retentation_period_days)))
