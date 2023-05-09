5d1629386be1d1f7ab9c4c5be37a300f8cd4f605
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2023-05-09 11:17:54 +0200

    build: add pre-commit and ruff config

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "requirements.txt")) as f:
 
 setup(
     name="alabcdk",
-    version="1.11.1",
+    version="1.12.0",
     description="Useful CDK constructs",
     url="https://github.com/aditrologistics/alabcdk.git",
     author="Jesper Högström",
